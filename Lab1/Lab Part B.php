3ad96a38ab70533b109f8b5e7018e6ebeadbe052
findlayhi <w0269118@nscc.ca>
2015-09-20 19:16:03 -0700

    New file part b working on.

---
+++ Lab1/Lab Part B.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+</head>
+
+<?php
+
+echo "<h1>  Greetings From Lab1 </h1>";
+?>
+
+<p>Deep v gluten-free readymade, Neutra tattooed skateboard 3 wolf moon. Biodiesel art party tofu, cardigan whatever migas street art wolf American Apparel next level. Tote bag keffiyeh selfies, heirloom leggings taxidermy kogi distillery scenester gluten-free drinking vinegar. Semiotics meggings photo booth cray single-origin coffee, Helvetica Intelligentsia Vice cold-pressed pork belly swag keytar Truffaut occupy lumbersexual. Small batch meggings squid, Banksy Austin cred blog leggings paleo meh tilde mumblecore Echo Park skateboard occupy. Pour-over aesthetic gluten-free High Life, fashion axe trust fund kogi McSweeney's iPhone forage. Semiotics farm-to-table pop-up authentic.</p>
+
+<?php
+
+echo "<h3>  The Ongoing Nature Of Lab1 </h3>";
+?>
+</br>
+<?php
+
+// this is the basic set up of declaring a value and outputting it
+$firstName = "Findlay Hilchie";
+echo $firstName;
+?>
+</br>
+
+<?php
+//this is an example of concatenation of a string
+$concatExample = "this" . "is" . "concatenation";
+echo $concatExample;
+?>
+</br>
+<?php
+//this is an example of math functions
+echo (32*14) + 83;
+echo "\r\n";
+echo (1024/128) - 7;
+echo "\r\n";
+echo 769%6;
+?>
+</br>
+
+<?php
+//loop to countdown
+$counter = 10;
+
+while ($counter != 0) {
+    echo "$counter </br>";
+    $counter -1;
+}
+
+?>
+
+
+
+
+
+</body>
+</html>
