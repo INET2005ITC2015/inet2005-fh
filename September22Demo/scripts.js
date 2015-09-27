22d830709ef6fbb37a72f0ee4f7b766156852b90
findlayhi <w0269118@nscc.ca>
2015-09-27 13:46:09 -0700

    I thought I did this

--- September22Demo/scripts.js
+++ September22Demo/scripts.js
@@ -1,8 +1,12 @@
 /**
  * Created by inet2005 on 9/22/15.
  */
+"use strict";
+
 function showMsg(currentElement)
 {
-   //alert('Got here');
+
+    var currentElement = document.body.childNodes[1].childNodes[1];
+   //get ellements we want to manipulate
     currentElement.innerHTML = "Changed Text";
 }
