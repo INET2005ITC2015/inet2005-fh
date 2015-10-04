bf1fbddcf386c888ea4b950c9c805b80c761cb9f
findlayhi <w0269118@nscc.ca>
2015-10-04 14:21:18 -0700

    forgot to add most of this in.

--- Lab3PHP/insertActor.php
+++ Lab3PHP/insertActor.php
@@ -1,7 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>First Ten Films</title>
+    <style type="text/css">
+        table, td, th {
+            border: 1px solid black;
+
+        }
+    </style>
+</head>
+<body>
+<table>
+    <thead>
+    <tr>
+        <th>First Name</th>
+        <th>Last Name</th>
+    </tr>
+    </thead>
+    <tbody>
 <?php
-/**
- * Created by PhpStorm.
- * User: inet2005
- * Date: 10/4/15
- * Time: 1:23 PM
- */
+$first = $_POST["first"];
+$last = $_POST["last"];
+
+require_once('dbConn.php');
+$db = getDBConnection();
+mysqli_query($db, "INSERT INTO actor (first_name, last_name) VALUES ($first, $last)");
+$result2 = mysqli_query($db, "SELECT * FROM actor");
+
+if (!$result2) {
+    die('Could not retrieve records from the Sakila Database: ' . mysqli_error($db));
+}
+
+while ($row = mysqli_fetch_assoc($result2))
+{
+    echo "<tr>";
+    echo "<td>" . $row['first_name'] . "</td>";
+    echo "<td>" . $row['last_name'] . "</td>";
+    echo "</tr>";
+
+}
+
+
+?>
+
+</tbody>
+</table>
+</body>
