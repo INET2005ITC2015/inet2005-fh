0b82728316ef38ecc534522b43dd57a653a2b790
findlayhi <w0269118@nscc.ca>
2015-10-05 11:45:24 -0700

    fixed redundant record insert issues but cannot seem to get the update to display when it has run. also for some reason it is inserting the first name entry as both first and last name when the record is created for an actor.

--- Lab3PHP/insertActor.php
+++ Lab3PHP/insertActor.php
@@ -20,39 +20,40 @@
     </tr>
     </thead>
     <tbody>
-<?php
-//will not pass for some reason
+    <?php
+    require_once('dbConn.php');
+    $db = getDBConnection();
+    if (isset($_POST['first']) && isset($_POST['last'])) {
+        $firstName = $_POST['first'];
+        $lastName = $_POST['last'];
 
-$firstName = $_POST['first'];
-$lastName = $_POST['last'];
+//echo "test" . $firstName . $lastNAme;
 
-echo "test" . $firstName;
+        $result = mysqli_query($db, "INSERT INTO actor (first_name, last_name)
+                               VALUES ('$firstName' , '$lastName')");
+    }
 
-require_once('dbConn.php');
-$db = getDBConnection();
+    $result2 = mysqli_query($db, "SELECT * FROM actor ORDER BY actor_id DESC LIMIT 10 ");
 
-$result=mysqli_query($db, "INSERT INTO actor (first_name, last_name)
-                    VALUES ('$firstName' , '$lastName')");
-$result2 = mysqli_query($db, "SELECT * FROM actor ORDER BY actor_id DESC LIMIT 10 ");
+    if (!$result2) {
+        die('Could not retrieve records from the Sakila Database: ' . mysqli_error($db));
+    }
 
-if (!$result2) {
-    die('Could not retrieve records from the Sakila Database: ' . mysqli_error($db));
-}
+    while ($row = mysqli_fetch_assoc($result2)) {
+        echo "<tr>";
+        echo "<td>" . $row['actor_id'] . "</td>";
+        echo "<td>" . $row['first_name'] . "</td>";
+        echo "<td>" . $row['first_name'] . "</td>";
+        echo "</tr>";
 
-while ($row = mysqli_fetch_assoc($result2))
-{
-    echo "<tr>";
-    echo "<td>" . $row['actor_id'] . "</td>";
-    echo "<td>" . $row['first_name'] . "</td>";
-    echo "<td>" . $row['first_name'] . "</td>";
-    echo "</tr>";
+    }
 
-}
+    $_POST['firstName'] = null;
+    $_POST['lastName'] = null;
 
+    ?>
 
-?>
-
-</tbody>
+    </tbody>
 </table>
 </br>
 <form action="deleteActor.php" method="post">
@@ -62,6 +63,8 @@ while ($row = mysqli_fetch_assoc($result2))
 </br>
 <form action="updateActor.php" method="post">
     ID To Update <input type="text" name="IdUpdate">
+    First Name <input type="text" name="firstName">
+    Last Name <input type="text" name="lastName">
     <input type="submit" value="Update">
 </form>
 </body>
--- Lab3PHP/updateActor.php
+++ Lab3PHP/updateActor.php
@@ -1,1 +1,13 @@
 <?php
+require_once('dbConn.php');
+$db = getDBConnection();
+$update = $_POST["IdUpdate"];
+$firstName = $_POST["firstName"];
+$lastName = $_POST["lastName"];
+
+
+mysqli_query($db, "UPDATE actor set first_name=$firstName AND last_name=$lastName WHERE  actor_id=$update");
+
+    echo 'Successfully updated ' . mysqli_affected_rows($db) . ' record(s)';
+    echo "</br>";
+    echo '<a href="insertActor.php">Back To Actor List</a>';
