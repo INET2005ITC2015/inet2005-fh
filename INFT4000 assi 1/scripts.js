d3797d974397bd8b92e2158bca7f479cbdd060fa
findlayhi <w0269118@nscc.ca>
2015-10-20 15:24:17 -0700

    have function to validate the entry and clear it if it is incorrect.

--- INFT4000 assi 1/scripts.js
+++ INFT4000 assi 1/scripts.js
@@ -4,3 +4,28 @@ function reset() {
     ('.subTable').empty()
 }
 
+function numcheck(element) {
+    var num = element.innerHTML;
+    if (!/^[1-9]$/.test(num)){
+        alert("Enter only numbers 1-9");
+       element.innerHTML = "";
+
+    }
+    else {
+
+
+    }
+
+
+}
+
+
+function doublechecker(){}
+
+var celllist = document.getElementsByClassName("edit");
+for(var i=0 ; i<celllist.length; i++)
+{
+    celllist[i].addEventListener('keyup', function(){
+        numcheck(this)
+    })
+}
