7a34a655000ab8d286fa5205823c4db4fcc69d7f
findlayhi <w0269118@nscc.ca>
2015-11-15 13:56:58 -0800

    students are stored in json at the start and those are then pushed into the working array so that they can be treated as objects the same as user entered data.

--- SpecialTopicsLab2/app.js
+++ SpecialTopicsLab2/app.js
@@ -1,52 +1,63 @@
 app=angular.module('studentApp',[])
 
-    app.controller('studentController',function($scope){
+app.controller('studentController',['$scope', '$http', function($scope, $http){
 
         $scope.students = []; // end of students array
 
-        function Student(id, fName, lName) {
-            this.id = id;
-            this.fName = fName;
-            this.lName = lName;
-        }
 
-        $scope.showPopper = function(){
-        $scope.showIt=true;
+            $http.get('students.json').success(function(data){
+                $scope.students.push.apply($scope.students, data);});
 
-        }
-        $scope.hidePopper = function(){
-            $scope.showIt=false;
 
-        }
+            function Student(id, fName, lName) {
+                this.id = id;
+                this.fName = fName;
+                this.lName = lName;
+            }
 
-        $scope.StudentChecker = function(id) {
-           var counter = 0;
-            for (counter; counter < $scope.students.length; counter++) {
-                if (id === $scope.students[counter].id) {
-                    return true
+
+
+            $scope.showPopper = function(){
+                $scope.showIt=true;
+
+            };
+            $scope.hidePopper = function(){
+                $scope.showIt=false;
+
+            };
+
+            $scope.StudentChecker = function(id) {
+                var counter = 0;
+                for (counter; counter < $scope.students.length; counter++) {
+                    if (id === $scope.students[counter].id) {
+                        return true
+                    }
                 }
-            }
-            return false
-        }
+                return false
+            };
 
-       $scope.addNewStudent = function() {
-            if(!$scope.StudentChecker($scope.id)){
-           this.studentToAdd = new Student($scope.id, $scope.fName, $scope.lName);
-           $scope.students.push({
-               id: this.studentToAdd.id,
-               fName: this.studentToAdd.fName,
-               lName: this.studentToAdd.lName
-
-           });//close push
-
-            }
-             else
-            { alert("That Id Exists Already")}
-
-           ;
+            $scope.addNewStudent = function() {
+                if ($scope.StudentChecker($scope.id)) {
+                    alert("That Id Exists Already")
 
 
-       }});
+                } else {
+
+        this.studentToAdd = new Student($scope.id, $scope.fName, $scope.lName);
+        $scope.students.push({
+            id: this.studentToAdd.id,
+            fName: this.studentToAdd.fName,
+            lName: this.studentToAdd.lName
+        });//close push
+
+                }
+
+
+
+
+            }}]);
+
+
 
 
 
