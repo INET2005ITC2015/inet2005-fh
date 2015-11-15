8e30abe8ae0cfd09e12f173fbae237aa09b3d1b1
findlayhi <w0269118@nscc.ca>
2015-11-15 15:32:18 -0800

    added mask functionality to force the use of long form student numbers.

--- SpecialTopicsLab2/app.js
+++ SpecialTopicsLab2/app.js
@@ -1,4 +1,4 @@
-app=angular.module('studentApp',['ui.grid'])
+app=angular.module('studentApp',['ui.mask','ui.grid'])
 
 app.controller('studentController',['$scope', '$http', function($scope, $http){
 
