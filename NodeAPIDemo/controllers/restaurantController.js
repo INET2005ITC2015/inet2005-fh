c86594ba61673176533bf002d8ac1a1a3e62f377
findlayhi <w0269118@nscc.ca>
2015-12-14 11:28:24 -0800

    all things in crud now working.

--- NodeAPIDemo/controllers/restaurantController.js
+++ NodeAPIDemo/controllers/restaurantController.js
@@ -2,10 +2,32 @@ var mongoose   = require('mongoose');
 mongoose.connect('mongodb://localhost/test/restaurants'); // connect to our database
 var Restaurant = require('../models/Restaurant');
 
+
+
+module.exports.index = function(req, res) {
+    Restaurant.find().limit(25).exec(function(err, restaurants) {
+        if (err)
+            res.send(err);
+
+        res.json(restaurants);
+    });
+
+};
+
 module.exports.store =function(req, res) {
 
     var restaurant = new Restaurant();      // create a new instance of the restaurant model
     restaurant.name = req.body.name;  // set the restaurants name (comes from the request)
+    restaurant.borough = req.body.borough;         // set the restaurants borough (comes from the request)
+    restaurant.cuisine = req.body.cuisine;         // set the restaurants cuisine (comes from the request)
+    restaurant.name = req.body.name;            // set the restaurants name (comes from the request)
+    restaurant.restaurant_id = req.body.restaurant_id;   // set the restaurants restaurant_id (comes from the request)
+    restaurant.grades.date = req.body.date;            // set the restaurants restaurant_id (comes from the request)
+    restaurant.grades.grade = req.body.grade;           // set the restaurants restaurant_id (comes from the request)
+    restaurant.grades.score = req.body.score;           // set the restaurants restaurant_id (comes from the request)
+    restaurant.address.building = req.body.building;        // set the restaurants restaurant_id (comes from the request)
+    restaurant.address.street = req.body.street;          // set the restaurants restaurant_id (comes from the request)
+    restaurant.address.zipcode = req.body.zipcode;         // set the restaurants restaurant_id (comes from the request)
     restaurant.save(function(err) {
         if (err)
             res.send(err);
@@ -15,15 +37,6 @@ module.exports.store =function(req, res) {
 
 };
 
-module.exports.index = function(req, res) {
-    Restaurant.find(function (err, restaurants) {
-        if (err)
-            res.send(err);
-
-        res.json(restaurants);
-    });
-
-};
 
 module.exports.show = function(req, res) {
     Restaurant.findById(req.params._id, function(err, restaurant) {
@@ -43,6 +56,9 @@ module.exports.update = function(req, res) {
             res.send(err);
 
         restaurant.name = req.body.name;  // update the restaurants info
+        restaurant.borough = req.body.borough;            // update the restaurants info
+        restaurant.cuisine = req.body.cuisine;            // update the restaurants info
+        restaurant.restaurant_id = req.body.restaurant_id;      // update the restaurants info
 
         // save the restaurant
         restaurant.save(function(err) {
@@ -64,7 +80,18 @@ module.exports.destroy = function(req, res) {
 
         res.json({ message: 'Successfully deleted' });
     });
-}
+
+    module.exports.search=function(req, res) {
+
+        Restaurant.findById(req.params.borough, function(err, restaurant) {
+
+            if (err)
+                res.send(err);
+
+            res.json(restaurant);
+        });
+    };
+};
 
 
 
--- NodeAPIDemo/routes/index.js
+++ NodeAPIDemo/routes/index.js
@@ -4,9 +4,10 @@ var restaurantController = require('../controllers/restaurantController');
 
 // middleware to use for all requests
 router.use(function(req, res, next) {
-    // do logging
-    console.log('Something is happening.');
-    next(); // make sure we go to the next routes and don't stop here
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+    next();
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
